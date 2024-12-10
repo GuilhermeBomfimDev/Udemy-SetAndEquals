d1ae2e1d419ae8aea79ee58778087ebf4bac7b0b
Guilherme Bomfim Dev <134552848+GuilhermeBomfimDev@users.noreply.github.com>
2024-12-10 17:55:49 -0300

    Add files via upload

---
+++ Udemy-Exercise-Set/Entities/Courses.cs
@@ -0,0 +1,28 @@
+﻿namespace Udemy_Exercise_Set.Entities
+{
+    public class Courses
+    {
+        public int Students { get; set; }
+        public string Course { get; set; }
+
+        public Courses(int students, string course)
+        {
+            Students = students;
+            Course = course;
+        }
+
+        public override int GetHashCode()
+        {
+            return Students.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Courses)) return false;
+
+            Courses other = obj as Courses;
+
+            return Students.Equals(other.Students);
+        }
+    }
+}
---
+++ Udemy-Exercise-Set/Program.cs
@@ -0,0 +1,38 @@
+﻿using Udemy_Exercise_Set.Entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HashSet<Courses> courses = new HashSet<Courses>();
+
+        Console.Write("How many students for course A? ");
+        int qtStudents = int.Parse(Console.ReadLine());
+        string course = "A";
+        SetStudents(qtStudents, course, courses);
+
+        Console.WriteLine();
+        Console.Write("How many students for course B? ");
+        qtStudents = int.Parse(Console.ReadLine());
+        course = "B";
+        SetStudents(qtStudents, course, courses);
+
+        Console.WriteLine();
+        Console.Write("How many students for course C? ");
+        qtStudents = int.Parse(Console.ReadLine());
+        course = "C";
+        SetStudents(qtStudents, course, courses);
+
+        Console.WriteLine();
+        Console.WriteLine("Total students: " + courses.Count);
+    }
+
+    static public void SetStudents(int qtStudents, string course, HashSet<Courses> courses)
+    {
+        for (int i = 0; i < qtStudents; i++)
+        {
+            int students = int.Parse(Console.ReadLine());
+            courses.Add(new Courses(students, course));
+        }
+    }
+}
